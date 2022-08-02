85b702cc0f531de890c47fd7fb5b9dff47475f03
ilijaepler <eplerilija17@gmail.com>
2022-08-02 22:10:49 +0200

    Implemented core idea of the logic, need to finish it up

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const gameBoard = (() => {
     let board = [
-        ['X', 'X', 'X'],
-        ['X', 'X', 'X'],
-        ['X', 'X', 'X']
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
     ];
 
     return {board};
@@ -15,6 +15,12 @@ const Player = (name, sign, onTurn) => {
 const displayController = (() => {
     let player1 = Player("player 1", 'X', true);
     let player2 = Player("player 2", 'O', false);
+
+    const gameLogic = (playerSign) => {
+        if(gameBoard.board[0][0] === playerSign && gameBoard.board[1][1] === playerSign && gameBoard.board[2][2] === playerSign){
+            console.log("Finished");
+        }
+    };
 
     const displayBoard = () => {
         const container = document.querySelector("#container");
@@ -34,9 +40,39 @@ const displayController = (() => {
                 column.style.display = "flex";
                 column.style.borderLeft = "1px solid black";
                 column.style.borderRight = "1px solid black";
-                column.textContent = gameBoard.board[i][j];
                 column.style.justifyContent = "center"
                 column.style.alignItems = "center"
+
+                let signHolder = document.createElement("div");
+                signHolder.style.display = "flex";
+                signHolder.style.justifyContent = "center";
+                signHolder.style.alignItems = "center"
+                signHolder.style.width = "50px";
+                signHolder.style.height = "50px";
+                signHolder.textContent = gameBoard.board[i][j];
+
+                column.appendChild(signHolder);
+
+                column.addEventListener("click", ()=>{
+                    if(gameBoard.board[i][j] === ""){
+                        if(player1.onTurn === true){
+                            signHolder.textContent = player1.sign;
+                            gameBoard.board[i][j] = player1.sign;
+                            console.log(gameBoard.board);
+                            player1.onTurn = false;
+                            player2.onTurn = true;
+                            gameLogic(player1.sign);
+                        }else{
+                            signHolder.textContent = player2.sign;
+                            gameBoard.board[i][j] = player2.sign;
+                            console.log(gameBoard.board);
+                            player2.onTurn = false;
+                            player1.onTurn = true;
+                            gameLogic(player2.sign);
+                        }
+                    }
+                    
+                })
 
                 row.appendChild(column);
             }
@@ -44,23 +80,16 @@ const displayController = (() => {
             container.appendChild(row);
         }
     };
-    /*player1.onTurn = true;
 
-    if(player1.onTurn === true){
-        // player1 turn
-        // implement the game logic
-        player1.onTurn = false
-    } else {
-        // player2 turn
-        // implement the game logic
-        player2.onTurn = false;
-    }*/
-    
     return {
+        displayBoard,
         player1,
-        player2,
-        displayBoard
+        player2
     };
 })();
 
 displayController.displayBoard();
+
+
+
+
