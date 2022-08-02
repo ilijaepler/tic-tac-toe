38aca88abcb7ed91d8dc423c778f509734a91932
ilijaepler <eplerilija17@gmail.com>
2022-08-02 15:51:08 +0200

    Added displayController module

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ const Player = (name, sign) => {
 
     return {name, sign, onTurn};
 };
+
+const displayController = (() => {
+    let player1 = Player("player 1", 'X');
+    let player2 = Player("player 2", 'O');
+    player1.onTurn = true;
+
+    if(player1.onTurn === true){
+        // player1 turn
+        // implement the game logic
+        player1.onTurn = false
+    } else {
+        // player2 turn
+        // implement the game logic
+        player2.onTurn = false;
+    }
+    
+})();
