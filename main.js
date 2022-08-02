90654766736c387eec515edae2f8fc531fd4c8f8
ilijaepler <eplerilija17@gmail.com>
2022-08-02 16:16:14 +0200

    Implemented function for drawing the board and reading the signs from board array

--- main.js
+++ main.js
@@ -8,16 +8,43 @@ const gameBoard = (() => {
     return {board};
 })();
 
-const Player = (name, sign) => {
-    let onTurn = false;
-
+const Player = (name, sign, onTurn) => {
     return {name, sign, onTurn};
 };
 
 const displayController = (() => {
-    let player1 = Player("player 1", 'X');
-    let player2 = Player("player 2", 'O');
-    player1.onTurn = true;
+    let player1 = Player("player 1", 'X', true);
+    let player2 = Player("player 2", 'O', false);
+
+    const displayBoard = () => {
+        const container = document.querySelector("#container");
+        for(let i = 0; i < 3; i++){
+            let row = document.createElement("div");
+            let rowId = "row" + i;
+            row.setAttribute("id", rowId);
+            row.style.flexGrow = "1";
+            row.style.display = "flex";
+            row.style.borderTop = "1px solid black";
+            row.style.borderBottom = "1px solid black";
+            for (let j = 0; j < 3; j++){
+                let column = document.createElement("div");
+                let columnId = rowId + "column" + j;
+                column.setAttribute("id", columnId);
+                column.style.flexGrow = "1";
+                column.style.display = "flex";
+                column.style.borderLeft = "1px solid black";
+                column.style.borderRight = "1px solid black";
+                column.textContent = gameBoard.board[i][j];
+                column.style.justifyContent = "center"
+                column.style.alignItems = "center"
+
+                row.appendChild(column);
+            }
+
+            container.appendChild(row);
+        }
+    };
+    /*player1.onTurn = true;
 
     if(player1.onTurn === true){
         // player1 turn
@@ -27,6 +54,13 @@ const displayController = (() => {
         // player2 turn
         // implement the game logic
         player2.onTurn = false;
-    }
+    }*/
     
+    return {
+        player1,
+        player2,
+        displayBoard
+    };
 })();
+
+displayController.displayBoard();
